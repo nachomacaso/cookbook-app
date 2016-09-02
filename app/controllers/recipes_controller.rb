f0832ac3225280f7e7c9d612a09f015cb9b4004f
nachomacaso <nathanmacaso@gmail.com>
2016-09-01 20:58:41 -0500

    Added Edit, Update, Create routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,60 @@
 class RecipesController < ApplicationController
-  def one
-    @recipes = Recipe.find(2)
+  def index
+    @recipes = Recipe.all
   end
 
-  def all
-    @recipes = Recipe.all
+  def new
+
+  end
+
+  def create
+    @recipe = Recipe.create(title: params[:title],
+                            chef: params[:chef],
+                            prep_time: params[:prep_time],
+                            ingredients: params[:ingredients],
+                            directions: params[:directions])
+    # Written individually
+    # @recipe.title = params[:title]
+    # @recipe.title = params[:chef]
+    # @recipe.title = params[:prep_time]
+    # @recipe.title = params[:ingredients]
+    # @recipe.title = params[:directions
+    # @recipe.save
+    render 'show.html.erb'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    # same as update
+    # @recipe.title = params[:title]
+    # @recipe.chef = params[:chef]
+    # @recipe.prep_time = params[:prep_time]
+    # @recipe.ingredients = params[:ingredients]
+    # @recipe.directions = params[:directions]
+    # @recipe.save
+
+    # this is also same as update
+    # @recipe.assign_attributes(title: params[:title],
+    #                           chef: params[:chef],
+    #                           prep_time: params[:prep_time],
+    #                           ingredients: params[:ingredients],
+    #                           directions: params[:directions])
+    # @recipe.save
+
+    @recipe.update(title: params[:title],
+                   chef: params[:chef],
+                   prep_time: params[:prep_time],
+                   ingredients: params[:ingredients],
+                   directions: params[:directions])
+
+    render 'show.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get '/one' => 'recipes#one'
-  get '/all' => 'recipes#all'
+  get '/' => 'recipes#index'
+  get '/recipes' => 'recipes#index'
+
+  get '/recipes/new' => 'recipes#new'
+  post '/recipes' => 'recipes#create'
+
+  get '/recipes/:id' => 'recipes#show'
+
+  get '/recipes/:id/edit' => 'recipes#edit'
+  patch '/recipes/:id' => 'recipes#update'
 end
