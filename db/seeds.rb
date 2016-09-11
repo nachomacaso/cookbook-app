c52cfd79f5690645bd6c0dd9e1149bc54ea46ac6
nachomacaso <nathanmacaso@gmail.com>
2016-09-11 11:39:25 -0500

    Added dropdown sort by attribute in navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+  {chef: "Really Bad Josh", title: "Hand Sandwich", ingredients: "left hand, right hand", directions: "put your hands together, Wisconsin!", image: nil, prep_time: 1},
+  {chef: "Cookie Monster", title: "Cookies", ingredients: "cookie mix, milk, eggs", directions: "pour mix into bowl, add milk and eggs, stir well, bake", image: nil, prep_time: 500},
+  {chef: "Capt. Kevin Bacon", title: "Bacon bits", ingredients: "bacon, regret", directions: "fry, flip, fry and cry", image: nil, prep_time: 3},
+  {chef: "Mac Tonight", title: "Moon Pies", ingredients: "pies, moon, dirt, drugs", directions: "put in a pan and bake, get stoned", image: nil, prep_time: 5}
+])
